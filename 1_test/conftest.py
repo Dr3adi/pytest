2f6ac2af55d5eb48767171231aed9c4fbf46ece8
Mukhin Artsem <artem.muxin@gmail.com>
2024-09-09 19:54:22 +0300

    finalizer

--- 1_test/conftest.py
+++ 1_test/conftest.py
@@ -2,5 +2,10 @@ import pytest
 
 
 @pytest.fixture
-def len_list():
-    return [1, 2, 3, 4, 5, 6, 7, 8]
+def len_list(request):
+    s = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    def finalizer():
+        print("\nfinalizer")
+    request.addfinalizer(finalizer)
+    return s
