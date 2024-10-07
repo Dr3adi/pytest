e86413f7261ced8108c9d3cca949fcfd850916b8
Mukhin Artsem <artem.muxin@gmail.com>
2024-10-07 20:25:58 +0300

    add new test and new api

--- 1_test/test_len.py
+++ 1_test/test_len.py
@@ -22,3 +22,23 @@ def test_dog_with_params(get_url_dog_api, count_images):
     data = url_api.json()
     print(f'\n{json.dumps(data, indent=4)}')
     assert len(data['message']) == count_images and data['status'] == 'success'
+
+
+@pytest.mark.parametrize("breeds_list", ['african', 'boxer', 'husky'])
+def test_breeds(get_url_dog_api, breeds_list):
+    new_url = f'{get_url_dog_api}/api/breed/{breeds_list}/images/random'
+    url_api = requests.get(new_url)
+    data = url_api.json()
+    assert breeds_list in new_url
+    assert data['status'] == 'success'
+
+
+# https://www.openbrewerydb.org/documentation/
+def test_city_filter():
+    url = 'https://api.openbrewerydb.org/v1/breweries'
+    params = {'by_city': 'san_diego', 'per_page': 3}
+    r = requests.get(url, params=params)
+    data = r.json()
+    assert len(data) == 3
+    for i in data:
+        assert i['city'] == 'San Diego'
